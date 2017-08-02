c21979fa8f278f8b5eb264d27ec5e71fb00ef0e8
terence <terence.delsate>
2017-08-02 15:08:29 +0200

    login working change password dropdown user menu

--- home_automation/apps/common/urls_commons.py
+++ home_automation/apps/common/urls_commons.py
@@ -1,8 +1,10 @@
-from django.conf.urls import url
-
-from .views_common import LoginView, index
+from django.conf.urls import url, include
+from django.urls import reverse_lazy
+from .views_common import LoginView, index, logout, change_password
 
 urlpatterns = [
-    url(r'^login/$', LoginView.as_view(), 'login'),
-    url(r'^index$', index, name='index')
+    url(r'^profile/login$', LoginView.as_view(), name='login'),
+    url(r'^profile/logout$', logout, name='logout'),
+    url(r'^profile/change_password', change_password, name = 'change_password'),
+    url(r'^$', index, name='index')
 ]
--- home_automation/apps/common/views_common.py
+++ home_automation/apps/common/views_common.py
@@ -1,24 +1,52 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import MenuItem
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse_lazy
 from django.views.generic.edit import FormView
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, PasswordResetForm
+from django.contrib.auth import authenticate, login, update_session_auth_hash
 from django.contrib.auth.views import login as view_login
+from django.contrib.auth.views import logout as view_logout
+from django.contrib.auth.views import password_change, PasswordChangeDoneView, password_reset, PasswordResetDoneView
 
 menu = MenuItem.objects.order_by('id')
 
+from django.contrib.auth.decorators import login_required
 
+
+@login_required(login_url=reverse_lazy('common:login'))
 def index(request):
     return render(request, 'common/index.html')
+
+
+def logout(request):
+    view_logout(request)
+    return redirect(reverse_lazy('common:login'))
+
+
+def change_password(request):
+    if request.method == "POST":
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)  # Important!
+            messages.success(request, 'Your password was successfully updated!', extra_tags='alert alert-success')
+            return redirect(reverse_lazy('common:change_password'))
+        else:
+            messages.error(request, 'Please correct the error below.', extra_tags='alert alert-danger')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'common/password_change.html', {'form': form})
 
 
 class LoginView(FormView):
     http_method_names = ['get', 'post']
     form_class = AuthenticationForm
-    template_name = 'profile/login.html'
+    template_name = 'common/login.html'
 
     def form_valid(self, form):
         username = form.cleaned_data['username']
